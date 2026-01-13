8b7c65f7493d4941c47244a6a6bbb0be7b783cfc
richard b mezzomo <richardmezzomo1@gmail.com>
2026-01-13 18:58:28 -0300

    ..

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'drizzle-kit'
-import { env } from './src/env'
+import { env } from "@/config/env";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	schema: './src/database/schema/**',
-	out: './src/database/migrations',
-	dialect: 'postgresql',
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-	casing: 'snake_case',
-})
+  schema: "./src/infra/db/drizzle/schema/**",
+  out: ".src/infra/db/drizzle/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  casing: "snake_case",
+});
